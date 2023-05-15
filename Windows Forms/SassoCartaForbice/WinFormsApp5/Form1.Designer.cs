ec625b3501728b69b7147bb5c68060754c0e5394
OmezzolliMarco <132301354+OmezzolliMarco@users.noreply.github.com>
2023-05-15 14:51:19 +0200

    Aggiornamento SassoCartaForbice

--- Windows Forms/SassoCartaForbice/WinFormsApp5/Form1.Designer.cs
+++ Windows Forms/SassoCartaForbice/WinFormsApp5/Form1.Designer.cs
@@ -35,6 +35,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -105,11 +110,65 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "label4";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sasso";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 381);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Carta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(203, 381);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Forbice";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(604, 91);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Salva";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(604, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -135,5 +194,10 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label5;
     }
 }
--- Windows Forms/SassoCartaForbice/WinFormsApp5/Form1.cs
+++ Windows Forms/SassoCartaForbice/WinFormsApp5/Form1.cs
@@ -4,25 +4,36 @@ namespace WinFormsApp5
     {
         int punti_player = 0;
         int punti_computer = 0;
+        string path = Directory.GetCurrentDirectory() + "\\classifica.txt";
         public Form1()
         {
             InitializeComponent();
 
             //codice da spostare
-            string path = Directory.GetCurrentDirectory() + "\\classifica.txt";
+            
             string daStampare = "Giocatore: 10 & Computer: 15";
             //per stampare la stringa sul file
-            File.WriteAllText(path, daStampare);
+            if(File.Exists(path))
+            {
+                //non fare niente
+                string contenuto = File.ReadAllText(path);
+                label5.Text = contenuto;
+            }
+            else
+            {
+                File.WriteAllText(path, ""); //creare il file nuovo ogni volta
+            }
+            
             //per aggiungere la stringa al file
-            File.AppendAllText(path, daStampare);
+            //File.AppendAllText(path, daStampare);
             //per leggere la stringa dal file
-            File.ReadAllText(path);
+            //File.ReadAllText(path);
 
             //metodo alternativo con using e streamWriter
-            using (StreamWriter writer = new StreamWriter(path))
-            {
-                writer.WriteLine(daStampare);
-            }
+            //using (StreamWriter writer = new StreamWriter(path))
+            //{
+            //    writer.WriteLine(daStampare);
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -92,6 +103,193 @@ namespace WinFormsApp5
 
             label3.Text = punti_player.ToString();
             label4.Text = punti_computer.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Properties.Resources.rock;
+
+            Random rand = new Random();
+            int player = 0; //genera 0,1 o 2
+            int computer = rand.Next(0, 3);
+
+            //0 -> sasso, 1 -> carta, 2 -> forbici
+
+            
+
+            if (computer == 0)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.rock;
+            }
+            else if (computer == 1)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.paper;
+            }
+            else if (computer == 2)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.scissor;
+            }
+
+            //0 -> sasso, 1 -> carta, 2 -> forbici
+            //controllo il vincitore
+            if (player == computer)
+            {
+                //lascio il punteggio invariato
+            }
+            else if (player == 0 && computer == 1)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 0 && computer == 2)
+            {
+                punti_player += 1;
+            }
+            else if (player == 1 && computer == 0)
+            {
+                punti_player += 1;
+            }
+            else if (player == 1 && computer == 2)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 2 && computer == 0)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 2 && computer == 1)
+            {
+                punti_player += 1;
+            }
+
+            label3.Text = punti_player.ToString();
+            label4.Text = punti_computer.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Properties.Resources.paper;
+
+            Random rand = new Random();
+            int player = 1; //genera 0,1 o 2
+            int computer = rand.Next(0, 3);
+
+            //0 -> sasso, 1 -> carta, 2 -> forbici
+
+
+
+            if (computer == 0)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.rock;
+            }
+            else if (computer == 1)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.paper;
+            }
+            else if (computer == 2)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.scissor;
+            }
+
+            //0 -> sasso, 1 -> carta, 2 -> forbici
+            //controllo il vincitore
+            if (player == computer)
+            {
+                //lascio il punteggio invariato
+            }
+            else if (player == 0 && computer == 1)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 0 && computer == 2)
+            {
+                punti_player += 1;
+            }
+            else if (player == 1 && computer == 0)
+            {
+                punti_player += 1;
+            }
+            else if (player == 1 && computer == 2)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 2 && computer == 0)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 2 && computer == 1)
+            {
+                punti_player += 1;
+            }
+
+            label3.Text = punti_player.ToString();
+            label4.Text = punti_computer.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Properties.Resources.scissor;
+
+            Random rand = new Random();
+            int player = 2; //genera 0,1 o 2
+            int computer = rand.Next(0, 3);
+
+            //0 -> sasso, 1 -> carta, 2 -> forbici
+
+
+
+            if (computer == 0)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.rock;
+            }
+            else if (computer == 1)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.paper;
+            }
+            else if (computer == 2)
+            {
+                pictureBox2.BackgroundImage = Properties.Resources.scissor;
+            }
+
+            //0 -> sasso, 1 -> carta, 2 -> forbici
+            //controllo il vincitore
+            if (player == computer)
+            {
+                //lascio il punteggio invariato
+            }
+            else if (player == 0 && computer == 1)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 0 && computer == 2)
+            {
+                punti_player += 1;
+            }
+            else if (player == 1 && computer == 0)
+            {
+                punti_player += 1;
+            }
+            else if (player == 1 && computer == 2)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 2 && computer == 0)
+            {
+                punti_computer += 1;
+            }
+            else if (player == 2 && computer == 1)
+            {
+                punti_player += 1;
+            }
+
+            label3.Text = punti_player.ToString();
+            label4.Text = punti_computer.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string stampa = "Player: " + punti_player.ToString() + "\nComputer: " + punti_computer.ToString();
+            File.WriteAllText(path, stampa);
+            label5.Text = File.ReadAllText(path);
         }
     }
 }
