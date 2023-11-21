8f8f5a6bb17f1ab0a663578592367659c9a055c6
OmezzolliMarco <omezzollimarco@gmail.com>
2023-11-21 16:09:44 +0100

    aggiunta file progetto scrittura e lettura su file

---
+++ Windows Forms/SerializzazioneJSON/SerializzazioneJSON/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace SerializzazioneJSON
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            string path = Directory.GetCurrentDirectory() + "\\test.txt";
+            string contenuto = File.ReadAllText(path);
+            Console.WriteLine(contenuto);
+            var coppie = contenuto.Split('\n');
+            foreach (string line in coppie)
+            {
+                Console.WriteLine(line);
+                var valori = line.Split(',');
+                int a = int.Parse(valori[0]);
+                int b = int.Parse(valori[1]);
+                int res = a + b;
+                Console.WriteLine(res);
+            }
+            //string path2 = Directory.GetCurrentDirectory() + "\\output.txt";
+            //string content = "Classe 4ASA";
+            //File.WriteAllText(path2, content);
+
+            //string a = "Ciao-a-tutti";
+            //var l = a.Split('-');
+
+
+            
+            //conversione oggetto a JSON
+            //Persona p = new Persona
+            //{
+            //    nome = "Mario",
+            //    cognome = "Rossi",
+            //    anno_di_nascita = 2000
+            //};
+            //string json = JsonConvert.SerializeObject(p);
+            //Console.WriteLine(json);
+
+            ////accesso al file
+            //string path = Directory.GetCurrentDirectory() + "\\persone.json";
+            //string contenuto = File.ReadAllText(path);
+            //List<Persona> lista = JsonConvert.DeserializeObject<List<Persona>>(contenuto);
+            //Console.WriteLine(lista);
+        }
+    }
+}
---
+++ Windows Forms/SerializzazioneJSON/SerializzazioneJSON/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializzazioneJSON
+{
+    internal class Persona
+    {
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public int anno_di_nascita { get; set; }
+        public Persona() 
+        {
+            
+        }
+    }
+}
