5279878287993a6510e3a0bf308586ce335c6b9b
OmezzolliMarco <omezzollimarco@gmail.com>
2023-11-28 16:12:22 +0100

    Aggiunta read/write
    Aggiunta progetto lettura e scrittura su files

---
+++ Windows Forms/LetturaScritturaFile/LetturaScritturaFile/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace LetturaScritturaFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(192, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SALVA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "nome 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "nome 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nomi salvati nel file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Windows Forms/LetturaScritturaFile/LetturaScritturaFile/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetturaScritturaFile
+{
+    public partial class Form1 : Form
+    {
+
+        //attributo di form1
+        string path = Directory.GetCurrentDirectory()+"\\test.txt";
+        public Form1()
+        {
+            InitializeComponent();
+
+            //carico informazioni salvate
+            if(File.Exists(path)) //input
+            {
+                //caricare le informazioni nel programma (elaborazione)
+                var contenutoFile = File.ReadAllText(path); //legge la stringa dal file
+                var nomi = contenutoFile.Split('-'); //array di String
+
+                label3.Text = nomi[0];
+                label4.Text = nomi[1];
+            }
+            else
+            {
+                //non carico le informazioni, ma creo il file predisposto
+                // per il salvataggio
+                var contenuto = ""; //creo stringa vuota da scrivere nel file
+                File.WriteAllText(path, contenuto);
+            }
+
+            /*inizio alternativo con try catch per lettura informazioni da file*/
+            try
+            {
+                //prova a fare qualcosa
+                var contenutoFile = File.ReadAllText(path); //legge la stringa dal file
+                var nomi = contenutoFile.Split('-'); //array di String
+
+                label3.Text = nomi[0];
+                label4.Text = nomi[1];
+            }
+            catch(Exception ex)
+            {
+                //gestisco l'errore
+                label3.Text="Player1";
+                label4.Text = "Player2";
+            }
+
+        }
+
+        //inizio di input
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //input
+            var nome1 = textBox1.Text;
+            var nome2 = textBox2.Text;
+
+            //elaborazione
+            var contenuto = nome1 + "-" + nome2;
+            File.WriteAllText(path, contenuto);
+
+            //output
+            var contenutoFile = File.ReadAllText(path); //legge la stringa dal file
+            var nomi = contenutoFile.Split('-'); //array di String
+
+            label3.Text = nomi[0];
+            label4.Text = nomi[1];
+        }
+    }
+}
