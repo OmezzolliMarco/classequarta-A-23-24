2528a73d7cdd68b71d6662f076f1269a872e8d26
OmezzolliMarco <132301354+OmezzolliMarco@users.noreply.github.com>
2023-12-13 09:47:33 +0100

    aggiornamento commenti

--- Windows Forms/SerializzazioneJSON/SerializzazioneJSON/Form1.cs
+++ Windows Forms/SerializzazioneJSON/SerializzazioneJSON/Form1.cs
@@ -32,30 +32,30 @@ namespace SerializzazioneJSON
                 int res = a + b;
                 Console.WriteLine(res);
             }
-            //string path2 = Directory.GetCurrentDirectory() + "\\output.txt";
-            //string content = "Classe 4ASA";
-            //File.WriteAllText(path2, content);
+            string path2 = Directory.GetCurrentDirectory() + "\\output.txt";
+            string content = "Classe 4ASA";
+            File.WriteAllText(path2, content);
 
-            //string a = "Ciao-a-tutti";
-            //var l = a.Split('-');
+            string a = "Ciao-a-tutti";
+            var l = a.Split('-');
 
 
             
-            //conversione oggetto a JSON
-            //Persona p = new Persona
-            //{
-            //    nome = "Mario",
-            //    cognome = "Rossi",
-            //    anno_di_nascita = 2000
-            //};
-            //string json = JsonConvert.SerializeObject(p);
-            //Console.WriteLine(json);
+            conversione oggetto a JSON
+            Persona p = new Persona
+            {
+               nome = "Mario",
+               cognome = "Rossi",
+               anno_di_nascita = 2000
+            };
+            string json = JsonConvert.SerializeObject(p);
+            Console.WriteLine(json);
 
-            ////accesso al file
-            //string path = Directory.GetCurrentDirectory() + "\\persone.json";
-            //string contenuto = File.ReadAllText(path);
-            //List<Persona> lista = JsonConvert.DeserializeObject<List<Persona>>(contenuto);
-            //Console.WriteLine(lista);
+            //accesso al file
+            string path = Directory.GetCurrentDirectory() + "\\persone.json";
+            string contenuto = File.ReadAllText(path);
+            List<Persona> lista = JsonConvert.DeserializeObject<List<Persona>>(contenuto);
+            Console.WriteLine(lista);
         }
     }
 }
